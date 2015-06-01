ca8d873d2c814a08538f4a243b667fc719319bed
sdcuike <sdcuike@users.noreply.github.com>
2015-06-01 11:38:39 +0800

    Esper Reference

---
+++ src/main/java/com/doctor/esper/reference/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * 			Esper Reference
+ * 				Version 5.2.0
+ *	 by Esper Team and EsperTech Inc. [http://esper.codehaus.org]
+ * 	 Copyright 2006 - 2015 by EsperTech Inc.
+
+ * @author doctor
+ *
+ * @time 2015年6月1日 上午11:36:38
+ */
+package com.doctor.esper.reference;
