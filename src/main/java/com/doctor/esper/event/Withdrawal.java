4cc31ea8073c2dfe46f4e8d7562780d09e47c300
sdcuike <sdcuike@users.noreply.github.com>
2015-06-01 18:46:13 +0800

    add Withdrawal

---
+++ src/main/java/com/doctor/esper/event/Withdrawal.java
@@ -0,0 +1,16 @@
+package com.doctor.esper.event;
+
+import java.math.BigDecimal;
+
+public class Withdrawal {
+	private BigDecimal amout;
+
+	public BigDecimal getAmout() {
+		return amout;
+	}
+
+	public void setAmout(BigDecimal amout) {
+		this.amout = amout;
+	}
+
+}
