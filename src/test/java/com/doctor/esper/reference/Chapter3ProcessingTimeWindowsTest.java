bc9e413e1ada335afb603c7471ac44eca47a65ed
sdcuike <sdcuike@users.noreply.github.com>
2015-06-02 15:45:19 +0800

    A.1. Introduction and Sample Data

--- src/test/java/com/doctor/esper/reference/Chapter3ProcessingTimeWindowsTest.java
+++ src/test/java/com/doctor/esper/reference/Chapter3ProcessingTimeWindowsTest.java
@@ -1,17 +1,21 @@
 package com.doctor.esper.reference;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.alibaba.fastjson.JSON;
 import com.doctor.esper.common.CommonUpdateListener;
 import com.doctor.esper.common.EsperUtil;
 import com.doctor.esper.event.Withdrawal;
 import com.espertech.esper.client.EPServiceProvider;
 import com.espertech.esper.client.EPStatement;
+import com.espertech.esper.client.EventBean;
 
 /**
  * Chapter 3 Processing Time Windows
@@ -49,11 +53,54 @@ public class Chapter3ProcessingTimeWindowsTest {
 
 		Withdrawal withdrawal = new Withdrawal("doctor", BigDecimal.valueOf(123.50D));
 		epServiceProvider.getEPRuntime().sendEvent(withdrawal);
+		List<EventBean> list = EsperUtil.get(epStatement);
+		log.info("{list:'{}'}", list);
 
 		withdrawal = new Withdrawal("doctor who ", BigDecimal.valueOf(23.50D));
 		epServiceProvider.getEPRuntime().sendEvent(withdrawal);
 		withdrawal = new Withdrawal("doctor who 2", BigDecimal.valueOf(23.50D));
 		epServiceProvider.getEPRuntime().sendEvent(withdrawal);
+		list = EsperUtil.get(epStatement);
+		log.info("{list:'{}'}", list);
+	}
+
+	/**
+	 * A.1. Introduction and Sample Data
+	 * 选用粒度5.5s间隔作为时间窗口，来演示失效策略事件流的进入和流出。
+	 * 
+	 * 查看输出内容和文档：事件数据集只保存最近5.5s时间窗口（FIFO缓存）。不在这个窗口期的事件过期。
+	 * 
+	 * @throws InterruptedException
+	 */
+	@Test
+	public void test_A_1_Introduction_and_Sample_Data() throws InterruptedException {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("select account,amount  ")
+				.append("from Withdrawal.win:time(5.5 sec) ");
+
+		String epl = stringBuilder.toString();
+		EPStatement epStatement = epServiceProvider.getEPAdministrator().createEPL(epl);
+		epStatement.addListener(CommonUpdateListener::update);
+
+		Withdrawal withdrawal = new Withdrawal("doctor", BigDecimal.valueOf(123.50D));
+		TimeUnit.SECONDS.sleep(2);
+		epServiceProvider.getEPRuntime().sendEvent(withdrawal);
+		List<EventBean> list = EsperUtil.get(epStatement);
+		log.info("{list:'{}'}", JSON.toJSONString(list));
+
+		withdrawal = new Withdrawal("doctor who ", BigDecimal.valueOf(23.50D));
+		TimeUnit.SECONDS.sleep(4);
+		epServiceProvider.getEPRuntime().sendEvent(withdrawal);
+		withdrawal = new Withdrawal("doctor who 2", BigDecimal.valueOf(23.50D));
+		TimeUnit.SECONDS.sleep(7);
+		epServiceProvider.getEPRuntime().sendEvent(withdrawal);
+
+		withdrawal = new Withdrawal("doctor who 3", BigDecimal.valueOf(123.50D));
+		withdrawal = new Withdrawal("doctor who 5", BigDecimal.valueOf(223.50D));
+		epServiceProvider.getEPRuntime().sendEvent(withdrawal);
+
+		list = EsperUtil.get(epStatement);
+		log.info("{list:'{}'}", JSON.toJSONString(list));
 
 	}
 
