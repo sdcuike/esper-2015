17395164a2f2f649e5c658ed6c71b79070239a5d
sdcuike <sdcuike@users.noreply.github.com>
2015-06-02 16:03:13 +0800

    Output Rate Limiting - Default

--- src/test/java/com/doctor/esper/reference/Chapter3ProcessingTimeWindowsTest.java
+++ src/test/java/com/doctor/esper/reference/Chapter3ProcessingTimeWindowsTest.java
@@ -104,4 +104,53 @@ public class Chapter3ProcessingTimeWindowsTest {
 
 	}
 
+	/**
+	 * A.2. Output for Un-aggregated and Un-grouped Queries
+	 * A.2.2. Output Rate Limiting - Default
+	 * 
+	 * With an output clause, the engine dispatches to listeners when the output condition occurs.
+	 * Here, the output condition is a 1-second time interval.
+	 * The engine thus outputs every 1 second, starting from the first event,
+	 * 
+	 * even if there are no new events or no expiring events to output.
+	 * 
+	 * The default (no keyword) and the ALL keyword result in the same output.
+	 * 
+	 * 我们可以用output语句，让引擎在output 条件触发时候，触发监听器。
+	 * 这里，我们设置output条件为1秒时间间隔。引擎每当1秒间隔，就会把从第一个事件开始传递并触发监听器，
+	 * 即使没有新事件或者过去事件要触发也会执行该操作。
+	 * 
+	 * @throws InterruptedException
+	 */
+	@Test
+	public void test_A_2_2_Output_Rate_Limiting_Default() throws InterruptedException {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("select account,amount  ")
+				.append("from Withdrawal.win:time(5.5 sec) ")
+				.append("output every 1 seconds");
+
+		String epl = stringBuilder.toString();
+		EPStatement epStatement = epServiceProvider.getEPAdministrator().createEPL(epl);
+		epStatement.addListener(CommonUpdateListener::update);
+
+		Withdrawal withdrawal = new Withdrawal("doctor", BigDecimal.valueOf(123.50D));
+		TimeUnit.SECONDS.sleep(2);
+		epServiceProvider.getEPRuntime().sendEvent(withdrawal);
+		List<EventBean> list = EsperUtil.get(epStatement);
+		log.info("{list:'{}'}", JSON.toJSONString(list));
+
+		withdrawal = new Withdrawal("doctor who ", BigDecimal.valueOf(23.50D));
+		TimeUnit.SECONDS.sleep(4);
+		epServiceProvider.getEPRuntime().sendEvent(withdrawal);
+		withdrawal = new Withdrawal("doctor who 2", BigDecimal.valueOf(23.50D));
+		TimeUnit.SECONDS.sleep(7);
+		epServiceProvider.getEPRuntime().sendEvent(withdrawal);
+
+		withdrawal = new Withdrawal("doctor who 3", BigDecimal.valueOf(123.50D));
+		withdrawal = new Withdrawal("doctor who 5", BigDecimal.valueOf(223.50D));
+		epServiceProvider.getEPRuntime().sendEvent(withdrawal);
+
+		list = EsperUtil.get(epStatement);
+		log.info("{list:'{}'}", JSON.toJSONString(list));
+	}
 }
