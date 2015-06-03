de9d23f4106626ad4744672496c48cba5db0abea
sdcuike <sdcuike@users.noreply.github.com>
2015-06-03 17:43:45 +0800

    add Chapter5EPLReferenceClausesTest

---
+++ src/test/java/com/doctor/esper/reference/Chapter5EPLReferenceClausesTest.java
@@ -0,0 +1,12 @@
+package com.doctor.esper.reference;
+
+/**
+ * Chapter 5. EPL Reference: Clauses
+ * 
+ * @author doctor
+ *
+ * @time 2015年6月2日 下午4:52:39
+ */
+public class Chapter5EPLReferenceClausesTest {
+
+}
