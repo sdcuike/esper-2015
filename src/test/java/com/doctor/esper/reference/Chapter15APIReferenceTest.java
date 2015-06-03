ca9541feca1607af4398cc7b95cdfedd100d9ba7
sdcuike <sdcuike@users.noreply.github.com>
2015-06-03 15:12:26 +0800

    15.5. On-Demand Fire-And-Forget Query Execution

--- src/test/java/com/doctor/esper/reference/Chapter15APIReferenceTest.java
+++ src/test/java/com/doctor/esper/reference/Chapter15APIReferenceTest.java
@@ -2,6 +2,7 @@ package com.doctor.esper.reference;
 
 import java.math.BigDecimal;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import org.junit.After;
 import org.junit.Before;
@@ -12,6 +13,9 @@ import org.slf4j.LoggerFactory;
 import com.alibaba.fastjson.JSON;
 import com.doctor.esper.common.EsperUtil;
 import com.doctor.esper.event.Withdrawal;
+import com.espertech.esper.client.EPOnDemandPreparedQuery;
+import com.espertech.esper.client.EPOnDemandPreparedQueryParameterized;
+import com.espertech.esper.client.EPOnDemandQueryResult;
 import com.espertech.esper.client.EPServiceProvider;
 import com.espertech.esper.client.EPStatement;
 
@@ -89,8 +93,66 @@ public class Chapter15APIReferenceTest {
 
 		withdrawal = new Withdrawal("doctor who", BigDecimal.valueOf(123.50D));
 		epServiceProvider.getEPRuntime().sendEvent(withdrawal);
+		log.info("{list:'{}'}", EsperUtil.get(epStatement));
 
 		//
+	}
+
+	/**
+	 * 15.5. On-Demand Fire-And-Forget Query Execution
+	 */
+	@Test
+	public void test_On_Demand_Fire_And_Forget_Query_Execution() {
+		StringBuilder window = new StringBuilder();
+		window.append("create window WithdrawalWindow.win:length(3)")
+				.append("select * from Withdrawal");
+		epServiceProvider.getEPAdministrator().createEPL(window.toString());
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append(" insert into WithdrawalWindow select *  ")
+				.append("from Withdrawal.win:length(3)");
+
+		String epl = stringBuilder.toString();
+		EPStatement epStatement = epServiceProvider.getEPAdministrator().createEPL(epl);
+		epStatement.setSubscriber(new WithdrawalSubscriber());
+
+		Withdrawal withdrawal = new Withdrawal("doctor", BigDecimal.valueOf(23.50D));
+		epServiceProvider.getEPRuntime().sendEvent(withdrawal);
+
+		withdrawal = new Withdrawal("doctor who", BigDecimal.valueOf(123.50D));
+		epServiceProvider.getEPRuntime().sendEvent(withdrawal);
+		withdrawal = new Withdrawal("doctor who 2", BigDecimal.valueOf(223.50D));
+		epServiceProvider.getEPRuntime().sendEvent(withdrawal);
+		withdrawal = new Withdrawal("doctor who 6", BigDecimal.valueOf(323.50D));
+		epServiceProvider.getEPRuntime().sendEvent(withdrawal);
+
+		// 15.5.1. On-Demand Query Single Execution
+		System.out.println("15.5.1. On-Demand Query Single Execution");
+		String query = "select * from WithdrawalWindow ";
+		EPOnDemandQueryResult queryResult = epServiceProvider.getEPRuntime().executeQuery(query);
+		System.out.println(queryResult.getArray().length);
+		Stream.of(queryResult.getArray()).forEach(System.out::println);
+
+		// 15.5.2. On-Demand Query Prepared Unparameterized Execution
+		System.out.println("15.5.2. On-Demand Query Prepared Unparameterized Execution");
+
+		EPOnDemandPreparedQuery preparedQuery = epServiceProvider.getEPRuntime().prepareQuery(query);
+		EPOnDemandQueryResult result = preparedQuery.execute();
+		System.out.println(result.getArray().length);
+		Stream.of(result.getArray()).forEach(System.out::println);
+
+		// 15.5.3. On-Demand Query Prepared Parameterized Execution
+		System.out.println("15.5.3. On-Demand Query Prepared Parameterized Execution");
+		query = "select * from WithdrawalWindow  where amount > ?";
+		EPOnDemandPreparedQueryParameterized queryParameterized = epServiceProvider.getEPRuntime().prepareQueryWithParameters(query);
+		queryParameterized.setObject(1, 200);
+		result = epServiceProvider.getEPRuntime().executeQuery(queryParameterized);
+		System.out.println(result.getArray().length);
+		Stream.of(result.getArray()).forEach(System.out::println);
+
+		queryParameterized.setObject(1, 300);
+		result = epServiceProvider.getEPRuntime().executeQuery(queryParameterized);
+		System.out.println(result.getArray().length);
+		Stream.of(result.getArray()).forEach(System.out::println);
 	}
 
 	/**
@@ -105,7 +167,11 @@ public class Chapter15APIReferenceTest {
 	 */
 	public static class WithdrawalSubscriber {
 		public void update(Withdrawal withdrawal) {
-			log.info(withdrawal.toString());
+			log.info("{new:'{}'}", withdrawal);
+		}
+
+		public void updateRStream(Withdrawal withdrawal) {
+			log.info("{old:'{}'}", withdrawal);
 		}
 
 		public void update2(Withdrawal withdrawal, long count) {
