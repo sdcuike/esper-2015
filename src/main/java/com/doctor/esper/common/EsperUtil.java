63bcd17c54ded7900530f260d153761931ef14c7
sdcuike <sdcuike@users.noreply.github.com>
2015-06-03 10:57:08 +0800

    add 15.2. comment The Service Provider Interface

--- src/main/java/com/doctor/esper/common/EsperUtil.java
+++ src/main/java/com/doctor/esper/common/EsperUtil.java
@@ -33,6 +33,19 @@ public enum EsperUtil {
 		return list;
 	}
 
+	/**
+	 * @see 15.2. The Service Provider Interface
+	 * 
+	 *      EPServiceProvider就代表一个esper引擎实例。 一个esper引擎的实例与其它引擎的实例是没有关联性的（独立性）。
+	 *      每个引擎的实例都有自己的administrative and runtime interface.
+	 *      一个引擎的实例有许多个，获得实例的EPServiceProviderManager方法为getDefaultProvider和getProvider(String providerURI)。
+	 *      后者可以用于根据不同的获得providerURI，引擎可以生成不同实例。EPServiceProviderManager根据providerURI存在与否，
+	 *      获取已经存在的实例或者新的实例。
+	 * 
+	 * 
+	 * @param config
+	 * @return
+	 */
 	public static EPServiceProvider esperConfig(String config) {
 		Configuration configuration = new Configuration();
 		configuration.configure(EsperUtil.class.getClassLoader().getResource(config));
