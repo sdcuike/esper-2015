b32ae17e20f43efcb877ef76d766245b36a70e03
sdcuike <sdcuike@users.noreply.github.com>
2015-06-03 16:41:02 +0800

    add Person class

---
+++ src/main/java/com/doctor/esper/event/Person.java
@@ -0,0 +1,45 @@
+package com.doctor.esper.event;
+
+import java.util.UUID;
+
+/**
+ * @author doctor
+ *
+ * @time 2015年6月3日 下午4:37:08
+ */
+public class Person {
+	private UUID id;
+	private String name;
+	private String firstName;
+	private String sex;
+	private int age;
+
+	public Person(String name, String firstName, String sex, int age) {
+		this.id = UUID.randomUUID();
+		this.name = name;
+		this.firstName = firstName;
+		this.sex = sex;
+		this.age = age;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+}
