401711ec72b837a68f7370cdb20dcc9399d1be45
sdcuike <sdcuike@users.noreply.github.com>
2015-06-09 14:47:20 +0800

    add hashCode equals method

--- src/main/java/com/doctor/esper/event/Person.java
+++ src/main/java/com/doctor/esper/event/Person.java
@@ -1,5 +1,6 @@
 package com.doctor.esper.event;
 
+import java.util.Objects;
 import java.util.UUID;
 
 import com.alibaba.fastjson.JSON;
@@ -17,10 +18,37 @@ public class Person {
 	private int age;
 
 	public Person(String name, String firstName, String sex, int age) {
-		this.id = UUID.randomUUID();
 		this.name = name;
 		this.firstName = firstName;
 		this.sex = sex;
+		this.age = age;
+	}
+
+	public Person(UUID id, String name, String firstName, String sex, int age) {
+		this.id = id;
+		this.name = name;
+		this.firstName = firstName;
+		this.sex = sex;
+		this.age = age;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public void setAge(int age) {
 		this.age = age;
 	}
 
@@ -42,6 +70,16 @@ public class Person {
 
 	public int getAge() {
 		return age;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(getId(), getName(), getFirstName(), getSex(), getAge());
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return this.hashCode() == obj.hashCode();
 	}
 
 	@Override
