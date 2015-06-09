ddc07313ec19082b8cc7bbca0b6b82cc099c68d7
sdcuike <sdcuike@users.noreply.github.com>
2015-06-09 16:05:04 +0800

    spring-esper 参看文档练习

---
+++ src/test/java/com/doctor/esper/reference_5_2_0/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * spring-esper 参看文档练习
+ * 
+ * @see http://www.espertech.com/esper/release-5.2.0/esper-reference/html_single/index.html
+ * 
+ * @author doctor
+ *
+ * @time 2015年6月9日 下午4:02:41
+ */
+package com.doctor.esper.reference_5_2_0;
