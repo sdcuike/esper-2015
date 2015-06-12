88104502af46dce81a32eeebd7faab23829d90bb
sdcuike <sdcuike@users.noreply.github.com>
2015-06-12 17:32:31 +0800

    ADD Chapter18IntegrationAndExtension

---
+++ src/test/java/com/doctor/esper/reference_5_2_0/Chapter18IntegrationAndExtension.java
@@ -0,0 +1,14 @@
+package com.doctor.esper.reference_5_2_0;
+
+/**
+ * Chapter 18. Integration and Extension
+ * 
+ * @author doctor
+ *
+ * @see http://www.espertech.com/esper/release-5.2.0/esper-reference/html_single/index.html#extension
+ *
+ * @time 2015年6月12日 下午3:19:55
+ */
+public class Chapter18IntegrationAndExtension {
+
+}
