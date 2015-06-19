16ab66e7b6d5d40592e9a82caf605b6b496fb974
sdcuike <sdcuike@users.noreply.github.com>
2015-06-19 18:03:04 +0800

    update to implements InitializingBean, DisposableBean

--- src/main/java/com/doctor/esper/spring/EsperTemplateBean.java
+++ src/main/java/com/doctor/esper/spring/EsperTemplateBean.java
@@ -1,10 +1,9 @@
 package com.doctor.esper.spring;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 
 /**
  * @see org.opencredo.esper.spring.EsperTemplateBean
@@ -13,17 +12,15 @@ import org.slf4j.LoggerFactory;
  *
  * @time 2015年6月8日 下午5:16:51
  */
-public class EsperTemplateBean extends EsperTemplate {
+public class EsperTemplateBean extends EsperTemplate implements InitializingBean, DisposableBean {
 	private final static Logger LOG = LoggerFactory.getLogger(EsperTemplateBean.class);
 
-	@PostConstruct
 	public void afterPropertiesSet() {
 		LOG.debug("Initializing the esper template bean");
 		super.initialize();
 		LOG.debug("Completed initializing the esper template bean");
 	}
 
-	@PreDestroy
 	public void destroy() {
 		LOG.debug("Destroying the esper template bean");
 		super.cleanup();
